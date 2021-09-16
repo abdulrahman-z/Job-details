4e98a37bd41df80bd3208afdd7d8102b61ce7833
rahman_z05 <mailtorahman.z@gmail.com>
2021-09-17 03:07:29 +0530

    styles correction - mobile view

--- src/JobPortal/Details.js
+++ src/JobPortal/Details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Styles from './Details.module.css'
-import workspace from '../Assets/workspace.jpg'
-import snapchat from '../Assets/snapchat.svg'
+import workspace from '../Assets/workspace2.jpg'
+import snapchat from '../Assets/snap-final.png'
 
 function Details() {
     return (
@@ -18,10 +18,15 @@ function Details() {
                 </div>
 
                 <div className={Styles.contentsContainer}>
-                    <h1 className={Styles.mainTitle}>Snapchat</h1>
-                    <p className={Styles.address}>SP Infocity Street, Chennai</p>
+                   
+                    <div className={Styles.titleContainer}>
+                         <h1 className={Styles.mainTitle}>Snapchat</h1>
+                        <p className={Styles.address}>SP Infocity Street, Chennai</p>
+                    </div>
+
                     <div className={Styles.contentBox}>
                         <div className={Styles.box}>
+                            
                             <h3 className={Styles.role}>Role</h3>
                             <h5 className={Styles.roleTitle}>Frontend Developer</h5>
                         </div>
@@ -36,67 +41,68 @@ function Details() {
                     </div>
 
                     <div className={Styles.overviewContainer}>
-                    <h1 className={Styles.heading}>
-                        Overview
-                    </h1>
-                    <div className={Styles.textContent}>
-                        <p className={Styles.paragraphText}>
-                        Snapchat is an American multimedia messaging app developed by Snap Inc., originally Snapchat Inc. 
-                        One of the principal features of Snapchat is that pictures and messages are usually only available 
-                        for a short time before they become inaccessible to their recipients.
-                        </p>
-                    </div>
-                </div>
+                            <h1 className={Styles.heading}>
+                                Overview
+                            </h1>
+                            <div className={Styles.textContent}>
+                                <p className={Styles.paragraphText}>
+                                Snapchat is an American multimedia messaging app developed by Snap Inc., originally Snapchat Inc. 
+                                One of the principal features of Snapchat is that pictures and messages are usually only available 
+                                for a short time before they become inaccessible to their recipients.
+                                </p>
+                            </div>
+                     </div>
                 
-                <div className={Styles.descriptionContainer}>
-                    <h1 className={Styles.descriptionTitle}>Job Description</h1>
-                    <ul className={Styles.descriptionLists}>
-                        <li className={Styles.descriptionListItem}>
-                        Minimum 2+ years experience in Front End Development
-                        </li>
-                        <li className={Styles.descriptionListItem}>
-                        Strong knowledge of Android SDK, different versions of Android, and how to deal with different screen
-                        </li>
-                        <li className={Styles.descriptionListItem}>
-                        Familiarity with cloud messaging APIs and push notifications
-                        </li>
-                        <li className={Styles.descriptionListItem}>
-                        Sound knowledge on RESTful and GraphQL APIs
-                        </li>
-                        <li className={Styles.descriptionListItem}>
-                        Proficient in Git and familiarity with continuous integration
-                        </li>
-                    </ul>
-                </div>
+                    <div className={Styles.descriptionContainer}>
+                            <h1 className={Styles.descriptionTitle}>Job Description</h1>
+                            <ul className={Styles.descriptionLists}>
+                                <li className={Styles.descriptionListItem}>
+                                Minimum 2+ years experience in Front End Development
+                                </li>
+                                <li className={Styles.descriptionListItem}>
+                                Strong knowledge of Android SDK, different versions of Android, and how to deal with different screen
+                                </li>
+                                <li className={Styles.descriptionListItem}>
+                                Familiarity with cloud messaging APIs and push notifications
+                                </li>
+                                <li className={Styles.descriptionListItem}>
+                                Sound knowledge on RESTful and GraphQL APIs
+                                </li>
+                                <li className={Styles.descriptionListItem}>
+                                Proficient in Git and familiarity with continuous integration
+                                </li>
+                            </ul>
+                        </div>
 
                 
                 
-                <div className={Styles.languagesContainer}>
-                    <h1 className={Styles.languageHeading}>Programming Languages</h1>
-                    <div className={Styles.languageBox}>
+                     <div className={Styles.languagesContainer}>
+                            <h1 className={Styles.languageHeading}>Programming Languages</h1>
+                            <div className={Styles.languageBox}>
 
-                        <button className={Styles.language}>HTML</button>
-                        <button className={Styles.language}>CSS</button>
-                        <button className={Styles.language}>Javascript</button>
-                        <button className={Styles.language}>JQuery</button>
-                        <button className={Styles.language}>Python</button>
-                        <button className={Styles.language}>Go</button>
-                        <button className={Styles.language}>Ruby</button>
-                  
-                    </div>
-                </div>
+                                <button className={Styles.language}>HTML</button>
+                                <button className={Styles.language}>CSS</button>
+                                <button className={Styles.language}>Javascript</button>
+                                <button className={Styles.language}>JQuery</button>
+                                <button className={Styles.language}>Python</button>
+                                <button className={Styles.language}>Go</button>
+                                <button className={Styles.language}>Ruby</button>
+                        
+                            </div>
+                     </div>
 
-                <div className={Styles.toolsContainer}>
-                    <h1 className={Styles.toolsHeading}>Tools</h1>
-                    <div className={Styles.toolBox}>
-                    <button className={Styles.tool}>React</button>
-                    <button className={Styles.tool}>Vue.js</button>
-                    <button className={Styles.tool}>Bootstrap</button>
-                    <button className={Styles.tool}>TailwindCSS</button>
-                    <button className={Styles.tool}>Codepen</button>
-                    <button className={Styles.tool}>VS code</button>
-                    <button className={Styles.tool}>Version control</button>
+                    <div className={Styles.toolsContainer}>
+                        <h1 className={Styles.toolsHeading}>Tools</h1>
+                        <div className={Styles.toolBox}>
+                        <button className={Styles.tool}>React</button>
+                        <button className={Styles.tool}>Vue.js</button>
+                        <button className={Styles.tool}>Bootstrap</button>
+                        <button className={Styles.tool}>TailwindCSS</button>
+                        <button className={Styles.tool}>Codepen</button>
+                        <button className={Styles.tool}>VS code</button>
+                        <button className={Styles.tool}>Version control</button>
                     </div>
+
                 </div>
                 
 
